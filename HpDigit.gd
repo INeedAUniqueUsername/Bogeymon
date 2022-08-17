40becf9a2c9547f6ffc262790c6fbabd90db04ee
Alex Chen <achen115@ucr.edu>
2022-08-17 15:05:26 -0700

    fix hproller

--- HpDigit.gd
+++ HpDigit.gd
@@ -4,27 +4,34 @@ extends Sprite
 export(float) var magnitude = 0
 export(float) var amount = 0 setget set_amount, get_amount
 export(bool) var truncate = false
-var prevDigit = 0
-var prevAmount = 0
+
+signal amount_changed()
 func set_amount(a):
-	var adj = fmod(a / pow(10, magnitude), 10)
-	#var adj = pow(10, magnitude)
 	if truncate:
-		var fl = floor(adj)
-		if fl != prevDigit:
-			var t = Tween.new()
+		var step = pow(10, magnitude)
+		var d = abs(a - amount)
+		if d >= step:
 			
-			t.interpolate_method(self, "set_rect_amount", prevDigit, fl, 0.4)
+	
+			var am = amount
+			
+			amount = a
+			emit_signal("amount_changed")
+			
+			var t = Tween.new()
+			t.interpolate_method(self, "set_amount_direct", am, a, 0.05 * d / step)
 			add_child(t)
 			t.start()
 			t.connect("tween_all_completed", t, "queue_free")
 			
-			prevDigit = fl
-			prevAmount = adj
+			connect("amount_changed", t, "queue_free")
 	else:
-		set_rect_amount(adj)
-	amount = a
+		amount = a
+		set_rect_amount(fmod(a / pow(10, magnitude), 10))
 func get_amount(): return amount
+func set_amount_direct(a):
+	amount = a
+	set_rect_amount(fmod(a / pow(10, magnitude), 10))
 
 func set_rect_amount(amount):
 	region_rect.position = Vector2(0, (10 - amount) * region_rect.size.y)
